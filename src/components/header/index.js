b10a5d7b0195103b084dc7e70a11af2cc04adfcf
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-12 15:30:42 -0800

    header code added to index

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+      <div className="container">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4 text-center">Office Space (Direcotry)</h1>
+            <h2 className="display-2 text-center">"So if you could sort all these employees, that would be greaaat."</h2>
+            <p className="lead text-center">SEARCH by first name, last name, email or phone</p>
+            <p className="lead text-center">or SORT by first name or email </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Header;
