711db138b8da592d261a8317c72aee0adc0ff2c4
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-12 15:55:50 -0800

    table function created

---
+++ src/components/table/index.js
@@ -0,0 +1,43 @@
+function Table(props) {
+
+  return (
+    <div className="container">
+      <table className="table table-striped table-hover justify-content-center">
+
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col"></th>
+          <th scope="col" onClick={props.sort}>First</th>
+          <th scope="col">Last</th>
+          <th scope="col" onClick={props.sort}>Email</th>
+          <th scope="col">Phone</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {props.randomUser.length > 0 ? props.randomUser.map(user => {
+          return(
+            <tr>
+              <th scope="row"><img src={user.picture.thumbnail} alt="Profile Thumbnail" /></th>
+              <td>{user.name.first}</td>
+              <td>{user.name.last}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+            </tr>
+          )
+        }):
+          <tr>
+            <th scope="row"></th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        }
+      </tbody>
+    </table>
+  </div>
+  )
+};
+
+export default Table;
