d5dd23e65a9ce655e08d77123160b0edbc2da50b
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-15 13:38:27 -0700

    added button

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,15 +1,16 @@
 function Form(props) {
   return (
     <div className="container">
-      <form className="form-inline my-2 my-lg-0 justify-content-end">
+      <form>
         <input
-          className="form-control mr-sm-2"
+          className="form-control col-lg-10"
           type="search"
           placeholder="Search Employees"
           aria-label="Search"
           name="search"
           value={props.employeeSearch}
           onChange={props.search} />
+          <button className="col-lg-2" type="submit" onClick={props.searchEmployees}>Search</button>
       </form>
     </div>
   )
