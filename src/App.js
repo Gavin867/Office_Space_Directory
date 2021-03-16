fc92e183b25098ede305bc90660417e7723c1751
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-15 17:02:00 -0700

    App.js format changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/header";
 import Form from "./components/form";
 import Table from "./components/table"
 import React, { Component } from "react";
-import api from "./utils/API"
+import api from "./utils/API";
 
 
 class App extends Component {
@@ -12,31 +12,44 @@ class App extends Component {
     search: "",
     originalemployees: [],
     sortAscend: true
-  }
+  };
+
   componentDidMount() {
     api.search()
       .then(response =>
-        this.setState({ 
-          employees: response.data.results, 
-          originalEmployees: response.data.results 
+        this.setState({
+
+          employees: response.data.results,
+
+          originalEmployees: response.data.results
+
         }))
       .catch(err => console.log(err));
   };
 
   handleInputChange = (event) => {
     const { name, value } = event.target;
-    
-    this.setState({ [name]: value })
-    console.log(value)
-    const newEmployee = this.state.originalEmployees.filter(employee => employee.name.first.toLowerCase().includes(value.toLowerCase()) || employee.name.last.toLowerCase().includes(value.toLowerCase()))
+
+    this.setState({ [name]: value });
+
+    console.log(value);
+
+    const newEmployee = this.state.originalEmployees.filter(
+
+      employee => employee.name.first.toLowerCase().includes(value.toLowerCase())
+
+        || employee.name.last.toLowerCase().includes(value.toLowerCase()))
+
     this.setState({ employees: newEmployee })
-  }
+  };
+
   handleSortChange = () => {
     const newEmployees = this.state.employees.sort((a, b) => a.name.first.localeCompare(b.name.first))
+    
     this.setState({ employees: newEmployees })
-  }
-  render() {
+  };
 
+  render() {
     return (
       <>
         <div>
@@ -49,8 +62,8 @@ class App extends Component {
           <Table employees={this.state.employees} handleSortChange={this.handleSortChange} />
         </div>
       </>
-    );
-  }
-}
+    )
+  };
+};
 
 export default App;
